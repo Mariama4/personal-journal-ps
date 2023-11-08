68563c38befc6b1a9f2d8795e27c97fe1ee30f9a
mariama4 <mariama4@mail.ru>
2023-11-08 20:29:45 +0700

    update Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,12 @@
 import './Button.css';
+import { memo } from 'react';
 
-function Button() {
-	return <button className="button  accent">hi</button>;
+function Button({ children, onClick }) {
+	return (
+		<button className="button accent" onClick={onClick}>
+			{children}
+		</button>
+	);
 }
 
-export default Button;
+export default memo(Button);
